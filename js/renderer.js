dd4c7ca37bdc115229a37d64b5c56c0bb9efce4b
vuquangpham <vuquangpham2909@gmail.com>
2024-02-11 01:28:16 +0700

    feat: renderer

--- js/renderer.js
+++ js/renderer.js
@@ -2,7 +2,10 @@ import * as THREE from "three";
 
 export default class Renderer {
   constructor(options) {
-    this.instance = new THREE.WebGLRenderer({ antialias: true });
+    this.instance = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: options.canvas,
+    });
     this.instance.setSize(options.width, options.height);
   }
 }
