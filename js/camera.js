0ab105d8dcde8a3be7b09efd434bd19c09608f66
vuquangpham <vuquangpham2909@gmail.com>
2024-02-11 14:29:10 +0700

    feat: camera

--- js/camera.js
+++ js/camera.js
@@ -1,8 +1,37 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default class Camera {
-  constructor(width, height) {
-    this.instance = new THREE.PerspectiveCamera(70, width, height, 0.01, 10);
-    this.instance.position.z = 1;
+  constructor(experience) {
+    this.experience = experience;
+    this.width = this.experience.sizes.width;
+    this.height = this.experience.sizes.height;
+
+    console.log(this.width, this.height);
+    this.setCamera();
+    this.setOrbitControls();
+  }
+
+  setCamera() {
+    this.instance = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.1,
+      100
+    );
+    this.instance.position.set(0, 0, 2);
+  }
+
+  setOrbitControls() {
+    this.controls = new OrbitControls(
+      this.instance,
+      this.experience.renderer.instance.domElement
+    );
+    this.controls.enableDamping = true;
+  }
+
+  updateControls() {
+    if (!this.controls) return;
+    this.controls.update();
   }
 }
