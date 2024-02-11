06a50854d624d58a0153675b24f8be24db96cf53
vuquangpham <vuquangpham2909@gmail.com>
2024-02-11 14:29:05 +0700

    feat: objects

--- js/objects.js
+++ js/objects.js
@@ -1,7 +1,17 @@
+import * as THREE from "three";
+
 export default class Objects {
-  constructor() {
+  constructor(experience) {
+    this.experience = experience;
+    this.scene = this.experience.scene;
+    this.camera = this.experience.camera.instance;
     this.init();
   }
 
-  init() {}
+  init() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: "red" });
+    const mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(mesh);
+  }
 }
